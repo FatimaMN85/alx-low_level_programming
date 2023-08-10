05d3e588bc6e5b8f6f670399b0d985030b85c92f
Fatima Moumni <toumita.fatmoumni@gmail.com>
2023-08-10 09:26:39 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+
 /**
  * main - this is my first function in C
  *
@@ -7,7 +8,6 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }    
