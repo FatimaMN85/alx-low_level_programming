470e7cd42c83c99ffc65877d2c509e84b9284946
Fatima Moumni <toumita.fatmoumni@gmail.com>
2023-08-11 14:08:39 -0700

    task 10  combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - prints all possible different combinations of two digits.
+ *
+ * Return: thiw function will return 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i <= 9)
+	{
+		int j;
+		j = i + 1;
+		while (j <= 9)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+
+			if (i != 8 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
