b2432759936fb3ed07095ef5a8f2eff9ab8e9036
Fatima Moumni <toumita.fatmoumni@gmail.com>
2023-08-11 11:45:48 -0700

    task 3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main -prints the alphabet in lowercase, and then in uppercase
+ *
+ *return: this function will return zero
+ */
+int main(void)
+{
+	char low, upp;
+
+	low = 'a';
+	upp = 'A';
+
+	while (low <= 'z')
+	{
+		putchar(low);
+		low++;
+	}
+
+	while (upp <= 'Z')
+	{
+		putchar(upp);
+		upp++;
+	}
+	putchar('\n');
+
+	return (0);
+}
