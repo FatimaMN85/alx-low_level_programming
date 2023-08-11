7eeb84600d3e3264bdd777791a7aa80a3f75457a
Fatima Moumni <toumita.fatmoumni@gmail.com>
2023-08-11 11:19:54 -0700

    the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - this function will prints the alphabet in lowercas.
+ *
+ * Return: this function will return zero
+ */
+int main(void)
+{
+	char ch;
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	
+	putchar('\n');
+
+	return (0);
+}
