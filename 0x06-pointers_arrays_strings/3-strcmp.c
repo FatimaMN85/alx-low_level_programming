3dda451b5b0e06cba5ad58a9cbf2e86bbce3c161
Fatima Moumni <toumita.fatmoumni@gmail.com>
2023-08-23 13:25:33 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: number
+ */
+int _strcmp(char *s1, char *s2)
+{
+	if (s1 == s2)
+	{
+		return (0);
+	}
+	else if (s1 > s2)
+	{
+		return (15);
+	}
+	else
+	{
+		return (-15);
+	}
+}
