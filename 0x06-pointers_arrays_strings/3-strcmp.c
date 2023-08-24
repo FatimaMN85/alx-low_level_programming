6f3edb096a859527a25ec72734e3caa6bb3c85c0
Fatima Moumni <toumita.fatmoumni@gmail.com>
2023-08-24 03:37:53 -0700

    compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,16 +9,10 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	if (s1 == s2)
+	while (*s1 && (*s1 == *s2))
 	{
-		return (0);
+		s1++;
+		s2++;
 	}
-	else if (s1 > s2)
-	{
-		return (15);
-	}
-	else
-	{
-		return (-15);
-	}
+	return *(unsigned char *)s1 - *(unsigned char *)s2;
 }
