be42c7d63f1a9e9b1a64d6930a8c6e2c697e7758
Fatima Moumni <toumita.fatmoumni@gmail.com>
2023-08-29 14:36:16 -0700

    init

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,7 +2,7 @@
 #include <stddef.h>
 
 /**
- * _strpbrk - searches a string for any of a set of bytes.
+ * *_strpbrk - searches a string for any of a set of bytes.
  * @s: first string
  * @accept: second string
  * Return: pointer
@@ -10,6 +10,7 @@
 char *_strpbrk(char *s, char *accept)
 {
 	int i, j;
+
 	if (*s == '\0' || *accept == '\0')
 	{
 		return ('\0');
