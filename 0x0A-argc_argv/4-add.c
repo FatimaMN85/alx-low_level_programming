a1d8c86c5ed5078bace58650ed56fce3400182ad
Fatima Moumni <toumita.fatmoumni@gmail.com>
2023-09-01 15:44:46 -0700

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int sum, num, i;
+
+	sum = 0;
+	i = 1;
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	while (i < argc)
+	{
+		num = atoi(argv[i]);
+		if (num == 0 && argv[i][0] != '0')
+		{
+			printf("Error\n");
+			return (1);
+		}
+		if (num > 0)
+		{
+			sum += num;
+		}
+		i++;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
