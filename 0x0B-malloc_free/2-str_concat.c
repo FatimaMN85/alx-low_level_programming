131e3400bea2336af011281bf00d9642342e769c
Fatima Moumni <toumita.fatmoumni@gmail.com>
2023-09-04 13:35:47 -0700

    concatenates

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,9 +13,13 @@ char *str_concat(char *s1, char *s2)
 	char *new_str;
 	size_t size;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
-		return "";
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
 	}
 	size = strlen(s1) + strlen(s2) + 1;
 	new_str = malloc(size * sizeof(char));
