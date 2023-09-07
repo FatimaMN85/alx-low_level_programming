204940da44106aedc5a196844b42157f00541d7e
Fatima Moumni <toumita.fatmoumni@gmail.com>
2023-09-07 09:37:50 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,6 +3,25 @@
 #include <string.h>
 
 /**
+ * my_memset - sets a block of memory to a specific value
+ * @ptr: pointer
+ * @val: the value
+ * @n_b: The number of bytes in the memory block
+ */
+void *my_memset(void *ptr, int val, size_t n_b)
+{
+	unsigned char *byt_ptr = (unsigned char *)ptr;
+	size_t i;
+
+	for (i = 0; i < n_b; i++)
+	{
+		byt_ptr[i] = (unsigned char)val;
+	}
+
+	return (ptr);
+}
+
+/**
  * _calloc - allocates memory for an array
  * @nmemb: the number of elements of array
  * @size: the size, in bytes, of each element in the array
@@ -22,7 +41,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		return (NULL);
 	}
-	memset(ptr, 0, nmemb * size);
+	my_memset(ptr, 0, nmemb * size);
 
 	return (ptr);
 }
