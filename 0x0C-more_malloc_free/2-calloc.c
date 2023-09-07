6925d16b735ea3c130453c3a73d0911783cb6356
Fatima Moumni <toumita.fatmoumni@gmail.com>
2023-09-07 14:24:07 -0700

    init

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -4,43 +4,41 @@
 
 /**
  * my_memset - sets a block of memory to a specific value
- * @ptr: pointer
+ * @str: string
  * @val: the value
  * @n_b: The number of bytes in the memory block
  */
-void *my_memset(void *ptr, int val, size_t n_b)
+void *my_memset(char *str, char val, unsigned int n_b)
 {
-	unsigned char *p = (unsigned char *)ptr;
-	size_t i;
+	unsigned int i;
 
 	for (i = 0; i < n_b; i++)
 	{
-		p[i] = val;
+		str[i] = val;
 	}
-	return (p);
+	return (str);
 }
 
 /**
  * _calloc - allocates memory for an array
  * @nmemb: the number of elements of array
  * @size: the size, in bytes, of each element in the array
- *
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr;
+	char *p;
 
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
-	ptr = malloc(nmemb * size);
+	p = malloc(nmemb * size);
 
-	if (ptr == NULL)
+	if (p == NULL)
 	{
 		return (NULL);
 	}
-	my_memset(ptr, 0, nmemb * size);
+	my_memset(p, 0, nmemb * size);
 
-	return (ptr);
+	return (p);
 }
