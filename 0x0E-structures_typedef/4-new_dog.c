a0e773d01ccec8c3e525c48499497a2a474ca6aa
Fatima Moumni <toumita.fatmoumni@gmail.com>
2023-09-12 08:25:16 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,15 +14,33 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
+	int len1, len2;
+
+	len1 = strlen(name);
+	len2 = strlen(owner);
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 	{
 		return (NULL);
 	}
-	new_dog->name = strdup(name);
+
+	new_dog->name = malloc(sizeof(char) * (len1 + 1));
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	new_dog->owner = malloc(sizeof(char) * (len2 + 1));
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog);
+		free(new_dog->name);
+		return (NULL);
+	}
+	strcpy(new_dog->name, name);
+	strcpy(new_dog->owner, owner);
 	new_dog->age = age;
-	new_dog->owner = strdup(owner);
 
 	return (new_dog);
 }
