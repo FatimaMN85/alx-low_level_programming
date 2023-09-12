2103023a4b39bf293aa93b61e98d3ce7aaeca64d
Fatima Moumni <toumita.fatmoumni@gmail.com>
2023-09-12 06:54:14 -0700

    Inside of a dog it's too dag

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,10 +12,11 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-} dog;
+} dog_t;
 
 /* function prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t new_dog(char *name, float age, char *owner);
 
 #endif
