b856d2b120777a3fdf3e2a181cea82d4568a1484
Fatima Moumni <toumita.fatmoumni@gmail.com>
2023-09-13 11:59:56 -0700

    init

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -4,17 +4,19 @@
 
 /**
  * array_iterator - executes a function
+ * @array: the array
  * @size: the size of the array
  * @action: pointer
  */
 void array_iterator(int *array, size_t size, void (*action)(int)){
 	size_t i;
 
-	if (array != NULL && action != NULL)
+	if (array == NULL && action == NULL)
 	{
-		for (i = 0; i < size; i++)
-		{
-			action(array[i]);
-		}
+		return;
+	}
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
 	}	
 }
