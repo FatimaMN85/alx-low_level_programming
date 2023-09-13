3dd8d10f0b35ffbb7ccc578f420612928fe90015
Fatima Moumni <toumita.fatmoumni@gmail.com>
2023-09-13 13:43:38 -0700

    init

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,9 +9,9 @@
  * @action: pointer
  */
 void array_iterator(int *array, size_t size, void (*action)(int)){
-	size_t i;
+	unsigned int i;
 
-	if (array == NULL && action == NULL)
+	if (array == NULL || action == NULL)
 	{
 		return;
 	}
