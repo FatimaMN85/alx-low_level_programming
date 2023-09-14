b9fb7db59cf9f8490cdd30dc0903c5daf8bc9995
Fatima Moumni <toumita.fatmoumni@gmail.com>
2023-09-14 06:44:25 -0700

    init

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,24 +11,29 @@
  */
 int main(int argc, char *argv[])
 {
-        int num1, num2;
-        int (*op_func)(int, int);
+	int num1, num2;
+	char *op;
 
-        if (argc != 4)
-        {
-                printf("Error\n");
-                return (98);
-        }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
 
-        num1 = atoi(argv[1]);
+	num1 = atoi(argv[1]);
+	op = argv[2];
 	num2 = atoi(argv[3]);
 
-	op_func = get_op_func(argv[2]);
-	if (op_func == NULL)
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		return (99);
 	}
-	printf("%d\n", op_func(num1, num2));
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(op)(num1, num2));
 	return (0);
 }
