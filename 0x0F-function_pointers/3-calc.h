17bfbb8f51d6056db9b27ef83e7450bd0a406c36
Fatima Moumni <toumita.fatmoumni@gmail.com>
2023-09-15 08:07:33 -0700

    init

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -12,12 +12,43 @@ typedef struct op
 	int (*f)(int a, int b);
 } op_t;
 
-
+/**
+ * op_add - return the sum of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the result
+ */
 int op_add(int a, int b);
+/**
+ * op_sub - return the sub of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the result
+ */
 int op_sub(int a, int b);
+/**
+ * op_mul - returns the product of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the result
+ */
 int op_mul(int a, int b);
+/**
+ * op_div - return the division of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the result
+ */
 int op_div(int a, int b);
+/**
+ * op_mod - return the reminder of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the result
+ */
 int op_mod(int a, int b);
+
+
 int (*get_op_func(char *s))(int, int);
 
 #endif
