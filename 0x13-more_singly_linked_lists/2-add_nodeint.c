ad49cd56b19b6fe7ae4a773c48caf47e0fbaa11f
Fatima Moumni <toumita.fatmoumni@gmail.com>
2023-09-25 08:30:47 -0700

    nodeint

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -18,11 +18,6 @@ listint_t *add_nodeint(listint_t **head, const int n)
 		return (NULL);
 	}
 	nNode->n = n;
-	if (nNode->n < 0)
-	{
-		free(nNode);
-		return (NULL);
-	}
 	nNode->next = *head;
 	*head = nNode;
 
